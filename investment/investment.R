75a4c2cb14cdae2a0ecbf2065ffc0fea971af8cc
Cait Robinson <57355504+CaitHRobinson@users.noreply.github.com>
2022-03-14 16:42:11 +0000

    Update investment.R

--- investment/investment.R
+++ investment/investment.R
@@ -7,21 +7,21 @@ library(graphics)
 investment <- read.csv("Investments_LA_bytype.csv")
 
 # Bee swarm plots
-beeswarm <- beeswarm(DepositsBanks_PopWeighted ~ Class, 
-         data= investment, 
-         pch=20, 
-         method="compactswarm", 
-         col = c("#808080", "#000000", "#D3D3D3","#808080", "#000000"),
-         corral = c("wrap"))
+beeswarm(DepositsBanks_PopWeighted ~ Class, # Variable partitioned by Local Government class
+                  data= investment, 
+                  pch=20, # Change to a point symbol
+                  method="compactswarm", # Make the swarm compact
+                  col = c("#808080", "#000000", "#D3D3D3","#808080", "#000000"), #m Black and white colour palette for printing
+                  corral = c("wrap")) # Wrap so that points cluster
 
-beeswarm <- beeswarm(DepositsBanks_PopWeighted ~ Class, 
+beeswarm(DepositsBanks_PopWeighted ~ Class, 
                      data= investment, 
                      pch=20, 
                      method="compactswarm", 
                      col = c("#808080", "#000000", "#D3D3D3","#808080", "#000000"),
                      corral = c("wrap"))
 
-beeswarm <- beeswarm(OtherInvestments_PopWeighted ~ Class, 
+beeswarm(OtherInvestments_PopWeighted ~ Class, 
                      data= investment, 
                      pch=20, 
                      method="compactswarm", 
